05e131019d54e3a34497303d93fa2e66e1656680
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-10-06 23:41:14 +0600

    Updated successfully

--- application/models/Car_model.php
+++ application/models/Car_model.php
@@ -14,7 +14,7 @@ class Car_model extends CI_model{
 		            ->get('car_models')
 		            ->result_array();
 
-		//SELECT * FROM car_models order by id ASC
+		//SELECT * FROM car_models order by id ASC (all)
 		return $result;            
 	}
 
